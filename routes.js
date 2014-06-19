1ab5819149f80a55afece4482e5455e6700940d0
Irina Chichikova <diva.artim@gmail.com>
2014-06-19 10:39:36 +0400

    Enables static content sharing
    Signed-off-by: Irina Chichikova <diva.artim@gmail.com>

--- routes.js
+++ routes.js
@@ -1,9 +1,12 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 var http = require('http').Server(app);
 
 var index = require('./controllers/index');
 var jedi = require('./controllers/jedi');
 var padawan = require('./controllers/padawan');
+
+app.use(express.static(__dirname + '/shared'));
 
 app.get('/', index.show);
 app.get('/jedi', jedi.show);
