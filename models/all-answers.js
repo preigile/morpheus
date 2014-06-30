cd80280eda6bc936a07048ee9939e18cfcdab650
Irina Chichikova <diva.artim@gmail.com>
2014-06-30 16:01:58 +0400

    Adds object when stubs data base. Exports this object.

---
+++ models/all-answers.js
@@ -0,0 +1,13 @@
+var allAnswers = {
+    answers: [
+        {term: 'Функция'},
+        {term: 'JavaScript', value: 'Язык программирования для Интернета'},
+        {term: 'Идентификатор'}
+    ],
+
+    getAllAnswers: function () {
+        return this.answers;
+    }
+};
+
+module.exports = allAnswers;
